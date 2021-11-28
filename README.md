da81e191e2d5702e241189bf54b45511ef5438ff
Radmir Gurkov <ragux0@gmail.com>
2021-11-28 20:32:44 +0100

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+## ffmpeg formatter
+
+### dependencies: ffmpeg should be installed on system
+
+Run:
+npm run start
+- add input file path
+- add output file path
+- add format default mp4
+
---
+++ index.js
@@ -0,0 +1,33 @@
+const prompt = require("prompt");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+
+prompt.start();
+
+prompt.get(
+  ["input", "output", { name: "format", default: "mp4" }],
+  (err, { input, output, format }) => {
+    console.group("Received");
+    console.log("m3u8 path", input);
+    console.log("mp4 path", output);
+    console.groupEnd();
+
+    ffmpeg(path.resolve(input))
+      .inputOptions("-protocol_whitelist file,http,https,tcp,tls,crypto")
+      .toFormat(format)
+      .save(path.resolve(output))
+      .on("error", (err) => console.error(err))
+      .on("start", function (commandLine) {
+        console.log("Spawned Ffmpeg with command: " + commandLine);
+      })
+      .on("progress", function (progress) {
+        console.group("----------------------");
+        console.log(JSON.stringify(progress));
+        console.groupEnd();
+      })
+      .on("end", function () {
+        console.log("Transcoding succeeded !");
+      })
+      .run();
+  }
+);
